5b36ec7a289e4fd44dc909a028413d9a65ec1d40
YuriyMokhov HomePC <mokhov86@yandex.ru>
2020-08-14 11:35:32 +0300

    1

--- src/app/balance/balance-element.ts
+++ src/app/balance/balance-element.ts
@@ -1,5 +1,16 @@
 export interface BalanceElement {
     name: string;
-    assets?: BalanceElement[],
-    liabilities?: BalanceElement[]
+    assets: BalanceElementAsset[],
+    liabilities: BalanceElementLiabilities[]
 }
+
+export interface BalanceElementAsset {
+    name: string;
+    value: number
+}
+
+export interface BalanceElementLiabilities {
+    name: string;
+    value: number
+}
+
--- src/app/balance/balance.component.ts
+++ src/app/balance/balance.component.ts
@@ -27,52 +27,41 @@ export class BalanceComponent implements OnInit {
         .addTo('body')
         .size(width, height);
 
+      const convasSettings = {
+        privateSectorBSNestedWidth: 540,
+        privateSectorBSNestedHeight: 320,
+        balanceElementNestedWidth: 170,
+        // balanceElementNestedHeight: 145
+      };
+
       let privateSectorBSNested = canvas.nested();
       privateSectorBSNested.attr({
-        fill: 'blue',
-        width: 300,
-        height: 300,
-        stroke: 'red'
+        id: 'privateSectorBSNested',
+        width: convasSettings.privateSectorBSNestedWidth,
+        height: convasSettings.privateSectorBSNestedHeight
+      });
+      privateSectorBSNested.rect(convasSettings.privateSectorBSNestedWidth, convasSettings.privateSectorBSNestedHeight).attr({
+        id: 'privateSectorBSNestedRect',
+        x: 0,
+        y: 0,
+        fill: 'white',
+        stroke: '#000',
       });
 
-      privateSectorBSNested.rect(100, 100).attr({
-        id: 'priBgRect',
-        x: 50,
-        y: 50,
-        fill: 'silver',
-        stroke: 'red',
-      });
-
-      // const privateSectorBSGroup = canvas.group()
-      //   .attr({
-      //     id: 'privateSectorBSGroup'
-      //   });
-
-      // let wrapper = privateSectorBSGroup.rect(540, 320).attr({
-      //   id: 'priBgRect',
-      //   x: 375,
-      //   y: 270,
-      //   fill: 'white',
-      //   stroke: '#000', 
-      // });
-
-      // //<text x="920" y="285" id="priScaleLabel" style="font-size: 12px; font-style: italic; font-family: arial,sans-serif; fill: black;">Scale: 1/1</text>
-      // privateSectorBSGroup
+      //<text x="920" y="285" id="priScaleLabel" style="font-size: 12px; font-style: italic; font-family: arial,sans-serif; fill: black;">Scale: 1/1</text>
+      // privateSectorBSNested
       //   .text('Целые сектора')
       //   .attr({
       //     id: 'priScaleLabel',
-      //     x: 920,
-      //     y: 285,
+      //     x: 0,
+      //     y: 0,
       //     style: 'font-size: 12px; font-style: italic; font-family: arial,sans-serif; fill: black;'
       //   });
 
 
-      // let bankBSGroup = privateSectorBSGroup.group().attr({
-      //   id: 'bankBS',
-      //   style: 'font-size: 9px; font-weight: bold; text-anchor: middle; font-family: arial,sans-serif;'
-      // });
 
-      // this.fillGroup(baseElements, bankBSGroup, wrapper);
+
+      this.fillBalanceElement(baseElements[0], privateSectorBSNested, 3);
 
 
     });
@@ -80,39 +69,67 @@ export class BalanceComponent implements OnInit {
 
   }
 
-  fillGroup(balanceElements: BalanceElement[], group: G, wrapper: Rect) {
-    let element = balanceElements[0];
-    //group lower line
-    group.line(wrapper.x(), wrapper.y(), 545, 571).attr({//group.line(375, 571, 545, 571).attr({
-      style: 'stroke:rgb(99,99,99);stroke-width:2'
+  fillBalanceElement(balanceElement: BalanceElement, parentSvg: Svg, countColumns: number) {
+
+    let svgHeight = 0; //Nested svg height for BalanceElement. Need calculate
+    let svgWidth = parentSvg.width() / countColumns; //Nested svg width for BalanceElement.
+
+    balanceElement.assets.forEach((asset) => { //Calculate svg height from the asset's values
+      svgHeight += asset.value;
     });
-    //group label
-    group.text('Банки').attr({
-      x: 460,
-      y: 580,
+    const topTextHeight = 20; //indent for top text 
+    const bottomTextHeight = 20;//indent for bottom text 
+    svgHeight = topTextHeight + svgHeight + bottomTextHeight; //total height
+
+    let nestedSvg = parentSvg.nested().attr({
+      id: `${balanceElement.name}NestedSvg`,
+      style: 'font-size: 9px; font-weight: bold; text-anchor: middle; font-family: arial,sans-serif;',
+      width: svgWidth,
+      height: svgHeight,
+      x: 0,
+      y: parentSvg.height() - svgHeight
+    });
+    nestedSvg.rect(nestedSvg.width(), nestedSvg.height()).attr({
+      id: `${balanceElement.name}NestedSvgRect`,
+      stroke: 'black',
+      fill: 'white'
+    });
+
+    //bottom line
+    nestedSvg.line(0, svgHeight - bottomTextHeight, svgWidth, svgHeight - bottomTextHeight).attr({
+      stroke: 'black',
+      'stroke-widht': 2
+    });
+    //bottom text
+    nestedSvg.text('Банки').attr({
+      x: svgWidth / 2,
+      y: svgHeight - bottomTextHeight,
       style: 'fill:black'
     });
+
+
+
     //group total result
-    group.text('$120').attr({
+    nestedSvg.text('$120').attr({
       x: 460,
       y: 445
     });
     //Assets text
-    group.text('Assets').attr({
+    nestedSvg.text('Assets').attr({
       x: 420,
       y: 445,
       fill: '#606060',
       'font-style': 'italic'
     });
     // Liabilities text
-    group.text('Liabilities').attr({
+    nestedSvg.text('Liabilities').attr({
       x: 500,
       y: 445,
       fill: '#606060',
       'font-style': 'italic'
     });
     //<rect width="70" height="40" stroke="black" stroke-width="1" fill="mediumblue" x="465" y="450"></rect>
-    group.rect(70, 40).attr({
+    nestedSvg.rect(70, 40).attr({
       stroke: 'black',
       'stroke-width': 1,
       fill: 'mediumblue',
@@ -120,14 +137,14 @@ export class BalanceComponent implements OnInit {
       y: 450
     });
     //<text style="fill: white;" x="500" y="460" visibility="true">Equity $40</text>
-    group.text('Equity $40').attr({
+    nestedSvg.text('Equity $40').attr({
       x: 500,
       y: 460,
       fill: 'white',
       visibility: true
     });
     //<rect width="70" height="40" stroke="black" stroke-width="1" fill="darkgreen" x="385" y="450"></rect>
-    group.rect(70, 40).attr({
+    nestedSvg.rect(70, 40).attr({
       stroke: 'black',
       'stroke-width': 1,
       fill: 'darkgreen',
@@ -135,13 +152,13 @@ export class BalanceComponent implements OnInit {
       y: 450
     });
     //<text style="fill: white;" x="420" y="460">Currency $40</text>
-    group.text('Currency $40').attr({
+    nestedSvg.text('Currency $40').attr({
       x: 420,
       y: 460,
       fill: 'white'
     });
     //<rect width="70" height="80" stroke="black" stroke-width="1" fill="darkgreen" x="385" y="490"></rect>
-    group.rect(70, 80).attr({
+    nestedSvg.rect(70, 80).attr({
       stroke: 'black',
       'stroke-width': 1,
       fill: 'darkgreen',
@@ -149,13 +166,13 @@ export class BalanceComponent implements OnInit {
       y: 490
     });
     //<text style="fill: white;" x="420" y="500">Reserves $80</text>
-    group.text('Reserves $80').attr({
+    nestedSvg.text('Reserves $80').attr({
       x: 420,
       y: 500,
       fill: 'white'
     });
     //<rect width="70" height="80" stroke="black" stroke-width="1" fill="darkred" x="465" y="490"></rect>
-    group.rect(70, 80).attr({
+    nestedSvg.rect(70, 80).attr({
       stroke: 'black',
       'stroke-width': 1,
       fill: 'darkred',
@@ -163,7 +180,7 @@ export class BalanceComponent implements OnInit {
       y: 490
     });
     //<text style="fill:white" x="500" y="500">Deposits $80</text>
-    group.text('Deposits $80').attr({
+    nestedSvg.text('Deposits $80').attr({
       x: 500,
       y: 500,
       fill: 'white'
